97041e35985f28a3a10f559a480595835967dd22
MicaelMunsfeld <106362082+MicaelMunsfeld@users.noreply.github.com>
2022-09-18 22:29:59 -0300

    Update insert.php

--- CRUD/insert.php
+++ CRUD/insert.php
@@ -1,53 +1,57 @@
-<?php 
+<?php
 
 include('./index.php');
 
-    $sql_insert_filme = "
-        INSERT into filmes
-        (nome,resumo,ano,complementos)
-        values
-        (:nome,:resumo,:ano,:complementos)
-    ";
-    $sql_insert_filme = $conexao->prepare($sql_insert_filme);
-
-    $dados = ([
-    	$nome        = $_POST['nome'],
-    	$resumo      = $_POST['resumo'],
-    	$ano         = $_POST['ano'],
-    	$complemento = $_POST['complemento']
-    ])
-
-
-    $sql_insert_filme->execute([
-    	$dados
-    ]);
+if(isset($_POST['cadastrar'])) {
+	try{
+		$stmt = $conexao->prepare("INSERT INTO filmes (nome, resumo, ano, complementos, imagem) VALUES (:nome,  :resumo, :ano, :complementos, :imagem);");
+		$stmt->bindParam(':nome'        , $_POST['nome'        ]);
+		$stmt->bindParam(':resumo'      , $_POST['resumo'      ]);
+		$stmt->bindParam(':ano'         , $_POST['ano'         ]);
+		$stmt->bindParam(':complementos', $_POST['complementos']);
+		$stmt->bindParam(':imagem'      , $_POST['imagem'      ]);
+		$stmt->execute();
+		header('Location: index.php?page=filmes');
+	} catch (PDOException $e) {
+		echo "<h2 class='danger'> Falha ao tentar cadastrar! Tente novamente. </h2>";
+	}
+}
 
 ?>
 
-<div class="container p-5">
-	<form action="" method="POST">
-		<div class="row">
-			<div class="col-6">
-				<div class="col-12">
-					<label>Nome</label>
-					<input type="text" name="nome" class="form-control">
+<div class="container mb-2">
+	<div class="row">
+		<h2 class="text-center">Cadastrar Filme:</h2>
+		<form method="POST" action="index.php?page=filmes">
+			<div class="row">
+				<div class="col-6">
+					<div class="form-group">
+						<label>Nome:</label>
+						<input required type="text" class="form-control" name="nome" id="nome"/>
+					</div>
+					<div class="form-group">
+						<label>Resumo:</label>
+						<input required type="text" class="form-control" name="resumo" id="resumo"/>
+					</div>
+					<div class="form-group">
+						<label>Ano:</label>
+						<input required type="number" class="form-control" name="ano" id="ano"/>
+					</div>
 				</div>
-				<div class="col-12">
-					<label>Resumo</label>
-					<input type="text" name="resumo" class="form-control">
+				<div class="col-6">
+					<div class="form-group">
+						<label>Imagem:</label>
+						<input required type="text" class="form-control" name="imagem" id="imagem"/>
+					</div>
+					<div class="form-group">
+						<label>Complementos:</label>
+						<input type="text" class="form-control" name="complementos" id="complementos"/>
+					</div>
 				</div>
-			</div>
-			<div class="col-6">
-				<div class="col-12">
-					<label>Ano</label>
-					<input type="number" name="ano" class="form-control">
+				<div class="d-grid mt-3">
+					<button class="btn btn-primary" type="submit" name="cadastrar">Cadastrar</button>
 				</div>
-				<div class="col-12">
-					<label>Complemento</label>
-					<textarea class="form-control" name="complemento" rows="4" cols="80"></textarea>
-				</div>
-			</div>
-			<button type="submit" class="btn btn-primary mt-3">Cadastrar</button>
+			</form>
 		</div>
-	</form>
+	</div>
 </div>
